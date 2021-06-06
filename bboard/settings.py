4035f74a0f6fe1b988132fc16c98ab58ae0f952d
Tereh71 <lositsky_v@palmira.ck.ua>
2021-06-06 15:46:28 +0300

    Error 1 migrate

--- bboard/settings.py
+++ bboard/settings.py
@@ -10,11 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -121,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'main.AdvUser'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import index
+from .views import other_page
 
 app_name ='main'
 urlpatterns = [
     path('', index, name='index'),
+    path('<str:page>/', other_page, name= 'other'),
+
 ]
