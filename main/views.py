2b51a8c4de6b6610bf6751670c30302c48277be1
Tereh71 <lositsky_v@palmira.ck.ua>
2021-06-13 23:05:50 +0300

    endeed 32.2

--- main/views.py
+++ main/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.decorators import login_reqyired
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def index (request):
@@ -16,9 +17,13 @@ def other_page(request, page):
     except TemplateDoesNotExist:
         raise Http404
     return HttpResponse(template.render(request=request))
+
 class BBLoginView(LoginView):
     template_name='main/login.html'
 
-@login_reqyired #декоратор @login_reqyired -  доступ до сторінки профілю надається тільки зареєстрованим користувачам
+@login_required #декоратор @login_reqyired -  доступ до сторінки профілю надається тільки зареєстрованим користувачам
 def profile(request):
     return render(request, 'main/profile.html')
+
+class BBLogoutView(LoginRequiredMixin, LogoutView):
+    template_name = 'main/logout.html'
